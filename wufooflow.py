4f24c5c9d0129887497a979c14e1360f757de8a6
Adam Cox <gadamc@cloudant.com>
2013-07-10 17:09:28 -0700

    adding form _id

--- wufooflow.py
+++ wufooflow.py
@@ -6,7 +6,7 @@ import json
 import requests
 import os
 import time
-from optparse import OptionParser
+from optparse import OptionParser #use optparse because running on python 2.6 
 
 TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 MAX_RETRIES = 10
@@ -18,6 +18,17 @@ db_user = None
 db_pass = None
 db_url = None
 
+
+def getLatestEntryInCloudant():
+    
+    r = requests.get(os.path.join(wf_url, 'users.json'), auth=(wf_apikey, 'foo'))
+    print json.dumps(json.loads(r.text), indent=1)
+
+def getEntriesFromWufoo( fromEntry=0):
+    pass
+
+def postEntriesToCloudant( docs ):
+    pass
 
 def main():
     global wf_user, wf_apikey, db_user, db_pass, db_url, wf_version
@@ -41,10 +52,12 @@ def main():
     db_user = config.get("Cloudant", 'db_user')
     db_pass = config.get("Cloudant", 'db_pass')
 
-   
-    r = requests.get(os.path.join(wf_url, 'users.json'), auth=(wf_apikey, 'foo'))
-    print json.dumps(json.loads(r.text), indent=1)
+    entryNum = getLatestEntryInCloudant()
 
+    formEntries = getEntriesFromWufoo( fromEntry = entryNum )
+
+    postEntriesToCloudant(formEntries)
+    
 
 if __name__ == '__main__':
     main()
